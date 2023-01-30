18d56a181c7b744c97ef02cbbe304795ad6dbda6
JennySarah45 <sarahsofiyaaaaa197@gmail.com>
2023-01-31 02:15:43 +0800

    updated

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -6,7 +6,13 @@ fp_write = Path.cwd()/"summary_report.txt"
 fp_write.touch()
 
 with fp_read.open(mode= "r", encoding = "UTF-8", newline = "") as file:
+    reader = csv.reader(file)
+    next(reader)
 
     Day = []
-    Cash_on_hand = []
-    
+    Cash_On_Hand = []
+
+    def total_calculated(option):
+        
+
+
