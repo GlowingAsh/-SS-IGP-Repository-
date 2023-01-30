709b04f0325dbc7147a1be805722ea1c08b37b48
GlowingAsh <112502141+GlowingAsh@users.noreply.github.com>
2023-01-31 00:00:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Code will connect all the three different functions into one file
+
+import cash_on_hand, overheads, profit_loss
+
+def main():
+  
+  overheads.overhead_function()
+  cash_on_hand.cash_on_hand_function()
+  profit_loss.profitloss_function()
+  
+ main()
