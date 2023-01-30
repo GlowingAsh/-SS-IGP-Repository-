6f678af2c8b4b5a8e1f6abbd5cd56e6a5b5f856b
JennySarah45 <sarahsofiyaaaaa197@gmail.com>
2023-01-31 02:02:31 +0800

    redid cash_on_hand file

---
+++ cash_on_hand.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import csv
+
+fp_read = Path.cwd()/"csv_reports/Cash_on_hand.csv"
+fp_write = Path.cwd()/"summary_report.txt"
+fp_write.touch()
+
+with fp_read.open(mode= "r", encoding = "UTF-8", newline = "") as file:
+    
