87a9dcd5c4f618cabb1e8f29123491e3b73717a6
GlowingAsh <chowjustin12@gmail.com>
2023-01-31 22:07:46 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -4,12 +4,12 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/ Profit_Loss.csv"
+fp = Path.cwd()/"csv_reports"/"Profit_Loss.csv"
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
   reader = csv.reader(file)
   next(reader) #skip header
   
   category = []
-  profit loss = []
+  profit_loss = []
   
