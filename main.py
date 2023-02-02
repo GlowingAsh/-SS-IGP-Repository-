8559d4cb7282a05304be10bc019bb68a4aa74fb5
ludwigkoh <123926898+ludwigkoh@users.noreply.github.com>
2023-02-02 16:34:42 +0800

    Update

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-# Code will connect all the three different functions into one file
-
-import cash_on_hand, overheads, profit_loss
+import cash_on_hand,overheads, profit_loss
 
 def main():
-  """
-  Function will integrate the different different functions higest_overheads, cash_on_hand and profitloss function
-  """
-  overheads.highest_overhead()
-  cash_on_hand.cash_on_hand_function()
-  profit_loss.profitloss_function()
-  
-  return main()
+  diff_cash = cash_on_hand.cash_on_hand_function()
+  max_overhead_val = overheads.overheads_function()
+  profit_losss = profit_loss.profit_loss_function()
 
+  summary = max_overhead_val + '\n'
 
-from pathlib import Path
-fp = Path.cwd()/"summary_report.txt" 
+  if(len(diff_cash))==0:
+    summary += '[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY'
+  else:
+    for i in range(len(diff_cash)):
+      summary +=  diff_cash[i] + '\n'
+  if(len(profit_losss))==0:
+    summary += '[NET PROFT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY '
+  else:
+    for i in range(len(profit_losss)):
+      summary +=  profit_losss[i] + '\n'
 
-with fp.open(mode="w", encoding="UTF-8") as file:
-  file.write(f"{main()}")
+  with open("summary_report.txt","a+") as f:
+    f.write(summary)
+  print("Data inserted Successfully in summary file") 
+
+main()
