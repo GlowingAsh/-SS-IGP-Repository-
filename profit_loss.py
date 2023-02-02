b9a664ef79576ac96db43352ad0c1062247697b7
GlowingAsh <chowjustin12@gmail.com>
2023-02-02 21:31:25 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -7,36 +7,48 @@ def profit_loss_function():
   Will compute the difference in the net profit column if the net profit on the current day is lower than the previous day.
   """
   
-  # Creates an empty list
+  # Make an empty list called profit_loss
   profit_loss = []
   
-  # # The Profit_Loss.csv file will be opened in read mode with UTF-8 encoding
+  # The Profit_Loss.csv file will be opened in read mode with UTF-8 encoding
   fp = Path.cwd()/"csv_reports"/"Profit_Loss.csv"
   
-  # Read the csv file 
+  # The file will be red and encoded in UTF-8 format 
   with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-  
+    
+    # Make a reader to read the file
     reader = csv.reader(file)
 
     # Skip header
     next(reader) 
+
     #starts counting the first row of the csv file
     line_count = 0
-   
+    
+    # Start with a value of 0
     prev_day_value = 0
     
     # A for loop is used in the reader to store the values of net profit into prev_day_value
     for row in reader:
+        # If this is the first row
         if line_count == 0:
+         # Add 1 to the line count
          line_count += 1 
+         # Store the value in the first row as prev_day_value
          prev_day_value = row[4]
+
         else:
-            if int(row[4]) < int(prev_day_value):
-                profit_loss.append(f'[PROFIT DEFICIT] DAY: {row[0]}, AMOUNT: USD{int(prev_day_value) - int(row[4])}') 
+            # If it's not the first row
+            # AND the line count is bigger than 1
+            # AND the current value is less than the previous value
+            if(line_count > 1 and (int(row[4]) < int(prev_day_val))): 
+                # The calculations will be stored and using an f string to dsiplay the values, then it will be appended to profit_loss 
+                profit_loss.append(f'[PROFIT DEFICIT] DAY: {row[0]}, AMOUNT: USD{int(prev_day_val) - int(row[4])}') 
             
-            elif int(row[4]) > int(prev_day_value):
-                profit_loss.append(f'[NET PROFIT SURPLUS] DAY: {row[0]}, AMOUNT: USD{int(row[4]) - int(prev_day_value)}')
-            line_count += 1 
-            prev_day_value = row[4]         
-  # Return keyword to return profit loss together with f-string
-  return profit_loss
+            # Add 1 to the line count
+            line_count += 1
+            # Previous day value is assigned to the 4th index  in the row
+            prev_day_val = row[4]  
+
+# Return the profit_loss list
+  return profit_loss 
