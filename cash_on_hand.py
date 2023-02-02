1c3e1c7305bba4898faf262d8cf9dfdc54585f3c
JennySarah45 <sarahsofiyaaaaa197@gmail.com>
2023-02-02 15:22:41 +0800

    update COH

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,9 +2,15 @@ from pathlib import Path
 import csv
 
 def cash_on_hand():
+    """
+    created a cash on hand function to calculate cash_on_hand
+    """
     prev_day = 0
+    #created a variable whereby the first day, previous day, will start from 0
     cash_deficit = []
+    # created a variable to calculate the cash deficit, if there was a cash deficit
     with open ('csv_reports\Cash_on_hand.csv') as csv_file:
+        # opening the csv file on cash on hand to use the data
         csv_Reader = csv.reader(csv_file, delimiter = ',')
         line_count = 0
 
