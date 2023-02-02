d1ea03f8b8af2f4b6fcb9e2e8b7e9a32d5755a52
GlowingAsh <chowjustin12@gmail.com>
2023-02-02 16:42:25 +0800

    Update profit_loss.py

--- profit_loss.py
+++ profit_loss.py
@@ -1,6 +1,8 @@
 # •	Profit & Loss csv : The program will compute the difference 
 # in the net profit column if net profit on the current day is lower than the previous day. 
 
+# ludwig is
+
 from pathlib import Path
 import csv
 
