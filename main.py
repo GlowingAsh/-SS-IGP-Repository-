150ba40a6357d98d3b50a273d56f416fb266b439
GlowingAsh <chowjustin12@gmail.com>
2023-02-02 21:31:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 import cash_on_hand, overheads, profit_loss
 
 def main():
-  """Function called main will inegrate the functions 
-  called cash_on_hand, overheads, profit_loss"""
+  """
+  Function called main will inegrate the functions 
+  called cash_on_hand, overheads, profit_loss
+  """
   
   # Variables are assigned to the various functions
   profit_losss = profit_loss.profit_loss_function()
-  diff_cash = cash_on_hand.cash_on_hand_function()
-  max_overhead_val = overheads.overheads_function()
+  cash_diff = cash_on_hand.cash_on_hand_function()
+  max_overhead_value = overheads.overheads_function()
 
+  # Max_overhead_value will be assigned to the summary variable, and creating a new line
+  summary = max_overhead_value + '\n'
 
-  summary = max_overhead_val + '\n'
-
- # Check if diff_cash is empty
-  if(len(diff_cash))==0:
+  # Check if cash_diff is empty
+  if(len(cash_diff))==0:
  
- # If diff_cash is empty, add the cash surplus message to the summary
+    # If diff_cash is empty, add the cash surplus message to the summary
     summary += '[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY'
 
   else:
-  # If diff_cash is not empty, add the diff_cash values to the summary
-    for i in range(len(diff_cash)):
-      summary +=  diff_cash[i] + '\n'
+    # If cash_diff is not empty, add the cash_diff values to the summary
+    for i in range(len(cash_diff)):
+      summary +=  cash_diff[i] + '\n'
   
   # Check if profit_loss is empty    
   if(len(profit_losss))==0:
   
-  # If profit_loss is empty, add the profit surplus message to the sumary 
+    # If profit_loss is empty, add the profit surplus message to the sumary 
     summary += '[NET PROFT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY '
   
   else:
@@ -43,7 +45,7 @@ def main():
   # Text file will be opened in write mode and is encoded in UTF-8 format
   with fp.open(mode="w", encoding="UTF-8") as file:
     
-  #The content within the variable "summary" will be written in the text file file.write()
+    # The content within the variable "summary" will be written in the text file file.write()
     file.write(summary)
 
 main()
