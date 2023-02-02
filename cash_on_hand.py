7ee48b521ebc2c24494b65867b19ed49e05ff1c0
aisyah <122858665+snoopyyzeus@users.noreply.github.com>
2023-02-02 16:58:37 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -11,8 +11,9 @@ def cash_on_hand_function():
     # created a variable to calculate the cash deficit, if there was a cash deficit
     # the empty list would be used to store the cash deficit
     
+    # create a file to csv file
     fp = Path.cwd()/"csv_reports\Cash_on_hand.csv"
-
+    
     with fp.open (mode="r", encoding="UTF-8", newline="") as file:
         # opening the csv file on cash on hand to use the data
         reader = csv.reader(file)
@@ -27,6 +28,7 @@ def cash_on_hand_function():
                 prev_day = row[1]
             else:
                 if (line_count > 1 and (int(row[1]) < int(prev_day))):
+                    # use '.append' to append the cash deficit as a list back to the empty list
                     cash_deficit.append(f'[CASH DEFICIT] DAY: {row[0]}, AMOUNT: USD{int(prev_day)- int(row[1])}')
                 prev_day = int(row[1])
                 line_count += 1
